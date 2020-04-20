cbc27ddb150d8fb572181552aa3088cea6c8126c
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-20 21:52:45 +0800

    routes/api folder created. auth.js, users.js, posts.js, profile.js route files and conenct to server.js

--- server.js
+++ server.js
@@ -7,5 +7,11 @@ ConnectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`[server.js]: running on PORT ${PORT}`));
