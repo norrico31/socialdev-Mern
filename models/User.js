2810f5466ddf4bd292f00b61f21814db98715999
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-21 19:16:02 +0800

    display all profile and single user profile in profile.js routes no auth all public

--- models/User.js
+++ models/User.js
@@ -23,4 +23,4 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('users', UserSchema);
+module.exports = mongoose.model('user', UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,7 +13,7 @@ const User = require('../../models/User');
 router.get('/me', auth, async (req, res) => {
     try {
 
-        const profile = await Profile.findOne({ user: req.user.id }).populate('users', ['name', 'avatar']);
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
         if(!profile) {
             return res.status(400).json({ msg: 'There is no profile for this user' });
         }
@@ -86,5 +86,39 @@ router.post('/', [ auth, [
         }
     }
 );
+
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Private
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Private
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
