5d24b9d762d61fa6d505229fc82cf6c37ac2558f
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-21 20:53:02 +0800

    router.put and router.delete method added for education in profile.js routes and access it using jsonwebtoken/auth middleware if the token is valid. add and delete education in model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -213,4 +213,77 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+// @route    Put api/profile/education
+// @desc     Add profile education
+// @access   Private
+router.put('/education', [ auth, [
+    check('school', 'School is required').not().isEmpty(),
+    check('degree', 'Degree is required').not().isEmpty(),
+    check('fieldofstudy', 'fieldofstudy date is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructure
+    const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    // Create an object with the data that the user submits
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+
+        const profile = await Profile.findOne({ user: req.user.id }); // req.user.id which we get from the jsonwebtoken/auth middleware
+        profile.education.unshift(newEdu); // object that we created above
+        await profile.save(); 
+        res.json(profile);
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
+
+// @route    DELETE api/profile/education/:edu_id
+// @desc     Delete education from profile
+// @access   Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const foundProfile = await Profile.findOne({ user: req.user.id });
+
+        // foundProfile.education = foundProfile.education.filter((exp) => exp._id.toString() !== req.params.edu_id);
+        // OR Get remove index
+        const removeIndex = foundProfile.education.map((item)=> item.id).indexOf(req.params.edu_id);
+        foundProfile.education.splice(removeIndex, 1);
+        await foundProfile.save();
+        res.json(foundProfile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
