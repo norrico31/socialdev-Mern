1b1064c10fedfc301a5753c7cc6d31881317d8af
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-21 18:53:19 +0800

    change users to user in models

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,7 +13,7 @@ const User = require('../../models/User');
 router.get('/me', auth, async (req, res) => {
     try {
 
-        const profile = await Profile.findOne({ user: req.user.id }).populate('users', ['name', 'avatar']);
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
         if(!profile) {
             return res.status(400).json({ msg: 'There is no profile for this user' });
         }
