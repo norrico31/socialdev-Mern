d995d61772e94cb08efa2deb2cf5f84c965e976e
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-21 20:20:55 +0800

    router.put method added in profile.js routes to update a experience user profile. auth middleware to protect the routes and send the jsonweb token to access the req.user.id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -141,4 +141,56 @@ router.delete('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 })
+
+
+// @route    Put api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put('/experience', [ auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructure
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    // Create an object with the data that the user submits
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+
+        const profile = await Profile.findOne({ user: req.user.id }); // req.user.id which we get from the jsonwebtoken/auth middleware
+        profile.experience.unshift(newExp); // object that we created above
+        await profile.save(); 
+        res.json(profile);
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
 module.exports = router;
