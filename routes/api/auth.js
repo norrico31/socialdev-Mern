3ecee135ac10ef74879b477dd62809837840995e
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-21 11:40:29 +0800

    authentication for backend is all set. We're able to login register get a token protect routes. auth middleware used as 2nd parameter to protect the routes. compare plain password from req.body to hash password from user.password(model)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -17,5 +21,49 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() }); // errors: errors.array() - is the 2nd parameter in check method above
+    }
+
+    const { email, password } = req.body;
+   
+    try {
+        let user = await User.findOne({ email: email });
+        if(!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }]});
+        }
+
+        // compare plain password from req.body to hash password from user.password(model)
+        const isMatch =  await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }]});
+        }
+
+        const payload = {
+            user: {
+                id: user.id   // user.id is from User model
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000000 }, (err, token) => {  // 3rd parameter is callback function to get the token
+            if(err) throw err;
+            res.json({ token })  // send the jwt token to json
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        res.send('Server Error');
+    }
+})
 
 module.exports = router;
