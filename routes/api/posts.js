906931deb24709fd30717f71fc06b97abaf235b1
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-21 21:53:41 +0800

    create a post using router.post in posts.js routes. auth middleware as 2nd parameter to protect the routes and to get the user id from req.user. find a user from User model and get the properties of user and create a instance of Post model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route    Get api/posts
-// @desc     Test route
-// @access   Public
-router.get('/', (req, res) => res.send('post route'));
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+
+
+// @route    Post api/posts
+// @desc     Create a post
+// @access   Private
+router.post('/', [ auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password'); // req.user.id is from auth that verified the token
+
+        // Instantiate of User model
+        const newPost = new Post({
+            user: req.user.id,
+            name: user.name,
+            avatar: user.avatar,
+            text: req.body.text      
+        });
+
+        const post = await newPost.save();
+        res.json(post);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
