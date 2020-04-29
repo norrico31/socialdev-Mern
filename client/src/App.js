c402c62f3cab2e6388c5940dd830102aa20fa9af
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-29 19:01:19 +0800

    PrivateRoute created at routing folder inside components folder to make the dashboard Private and use it into App.js and created a new PrivateRoute for dashboard.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 
+//Private Route
+import PrivateRoute from './components/routing/PrivateRoute';
+
+import Dashboard from './components/dashboard/Dashboard';
+
 import { loadUser } from './actions/auth';
 import { setAuthToken } from './utils/setAuthToken';
 
@@ -36,6 +41,7 @@ const App = () => {
                         <Switch>
                             <Route exact path="/register" component={ Register } />
                             <Route exact path="/login" component={ Login } />
+                            <PrivateRoute exact path="/dashboard" component={ Dashboard } />
                         </Switch>
                     </div>
             </Fragment>
