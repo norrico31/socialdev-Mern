f335bbbca289eab6b8643f9cdd62de3f785133db
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-29 21:46:56 +0800

    created ProfileForm.js in profile-forms components and add in the App.js. useState hooks used to handle all the input fields and onChange and onClick initialize

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Alert from './components/layout/Alert';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-forms/ProfileForm';
 
 import { loadUser } from './actions/auth';
 import { setAuthToken } from './utils/setAuthToken';
@@ -42,6 +43,7 @@ const App = () => {
                             <Route exact path="/register" component={ Register } />
                             <Route exact path="/login" component={ Login } />
                             <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+                            <PrivateRoute exact path="/create-profile" component={ ProfileForm } />
                         </Switch>
                     </div>
             </Fragment>
