add0bdcca13527471409b345bd8a4c796793d993
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-05-02 21:36:02 +0800

    minimal changes

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -8,13 +8,13 @@ const Experience = ({ experience }) => {
 
     const experiences = experience.map((exp) => (
         <tr key={exp._id}>
-        <td>{exp.company}</td>
-        <td className="hide-sm">{exp.title}</td>
-        <td><Moment format="YYYY/MM/DD">{moment.utc(exp.from)}</Moment> - {' '}{exp.to === null ? (' Now') : (<Moment format="YYYY/MM/DD">{moment.utc(exp.to)}</Moment>)}
-        </td>
-        <td>
-            <button className="btn btn-danger">Delete</button>
-        </td>
+            <td>{exp.company}</td>
+            <td className="hide-sm">{exp.title}</td>
+            <td><Moment format="YYYY/MM/DD">{moment.utc(exp.from)}</Moment> - {' '}{exp.to === null ? (' Now') : (<Moment format="YYYY/MM/DD">{moment.utc(exp.to)}</Moment>)}
+            </td>
+            <td>
+                <button className="btn btn-danger">Delete</button>
+            </td>
         </tr>
     ))
 
