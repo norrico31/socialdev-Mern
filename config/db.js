8c166cdfdd719bc5a94191df6b1b34ad42e98665
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-05-05 19:25:29 +0800

    kupal

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 exports.ConnectDB = async () => {
     try {
-        await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
-        console.log('MongoDB Connected...');
-    } catch (error) {
-        console.error(error.message);
+        const conn = await mongoose.connect(process.env.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
 
         // Exit process with failure
         process.exit(1);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { ConnectDB } = require('./config/db.js');
+const dotenv = require('dotenv');
 const path = require('path');
+dotenv.config({ path: './config/config.env' });
 const app = express();
 
-// Connect database
+// DB connection
+const { ConnectDB } = require('./config/db');
 ConnectDB();
 
 // Init Middleware
@@ -20,10 +22,8 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`[server.js]: running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`[server.js]: running on ${process.env.NODE_ENV} mode on PORT ${PORT}`));
