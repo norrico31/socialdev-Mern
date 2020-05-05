7f457f118e8278017700cd25349c3f0be75420f0
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-05-05 14:26:46 +0800

    CommentItem.js created at post folder inside components and put in Post.js parent component. postId passed in CommentItem from Post parent component to use in deleteComment. just adding some little snippets

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -20,7 +20,7 @@ const PostItem = ({ post: { _id, text, name, avatar, user, likes, comments, date
                         <button type="button" className="btn btn-light"><i className="fas fa-thumbs-up" onClick={(e) => addLike(_id) }></i>{' '}{likes.length > 0 && (<span>{likes.length}</span>)}</button>
                         <button type="button" className="btn btn-light"><i className="fas fa-thumbs-down" onClick={(e) => removeLike(_id) }></i></button>
 
-                        <Link to={`/post/${_id}`} className="btn btn-primary">Discussion{comments.length > 0 && (<span className='comment-count'>{comments.length}</span>)}</Link>
+                        <Link to={`/post/${_id}`} className="btn btn-primary">Discussion {' '}{comments.length > 0 && (<span className='comment-count'>{comments.length}</span>)}</Link>
 
                         {!auth.loading && user === auth.user._id && (
                             <button type="button" className="btn btn-danger" onClick={(e) => deletePost(_id)}><i className="fas fa-times"></i></button>
